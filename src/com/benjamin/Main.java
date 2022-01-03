9a4a6bdd2c5af205c33de007065efff9072fede8
Keith Benjamin <kjb528i@yahoo.com>
2022-01-03 18:42:45 -0500

    my update

--- src/com/benjamin/Main.java
+++ src/com/benjamin/Main.java
@@ -3,7 +3,7 @@ package com.benjamin;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("testing");
         System.out.println(GetFirstElement.getFirstValue(new int[]{1, 2, 3, 4}));
     }
 }
