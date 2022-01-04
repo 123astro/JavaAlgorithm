2b823e381d69c75229d1b8f4720e7cf0f4a7e4d0
Keith Benjamin <kjb528i@yahoo.com>
2022-01-04 10:35:14 -0500

    Added FindTheIndex algorithm.

--- src/com/benjamin/Main.java
+++ src/com/benjamin/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("testing");
         System.out.println(GetFirstElement.getFirstValue(new int[]{1, 2, 3, 4}));
-        System.out.println(( NextNumber.addition(2)));
+        System.out.println((NextNumber.addition(2)));
         System.out.println(ReturnSumOf2Num.sum(3, 2));
-
+        System.out.println(MinToSec.convert(6));
     }
 }
---
+++ src/com/benjamin/MinToSec.java
@@ -0,0 +1,7 @@
+package com.benjamin;
+
+public class MinToSec {
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+}
