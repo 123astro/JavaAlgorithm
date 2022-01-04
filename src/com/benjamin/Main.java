353be45c3a26279a0403bd2dc88151953140af9c
Keith Benjamin <kjb528i@yahoo.com>
2022-01-04 09:28:00 -0500

    Added sum of two numbers algorithm.

--- src/com/benjamin/Main.java
+++ src/com/benjamin/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("testing");
         System.out.println(GetFirstElement.getFirstValue(new int[]{1, 2, 3, 4}));
         System.out.println(( NextNumber.addition(2)));
+        System.out.println(ReturnSumOf2Num.sum(3, 2));
     }
 }
---
+++ src/com/benjamin/ReturnSumOf2Num.java
@@ -0,0 +1,7 @@
+package com.benjamin;
+
+public class ReturnSumOf2Num {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
