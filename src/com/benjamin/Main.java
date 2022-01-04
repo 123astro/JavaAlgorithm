2bc9f8473ef602891bc28bb6d3927ff31f482ee8
Keith Benjamin <kjb528i@yahoo.com>
2022-01-04 09:22:54 -0500

    added next number algorithm

--- src/com/benjamin/Main.java
+++ src/com/benjamin/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("testing");
         System.out.println(GetFirstElement.getFirstValue(new int[]{1, 2, 3, 4}));
+        System.out.println(( NextNumber.addition(2)));
     }
 }
---
+++ src/com/benjamin/NextNumber.java
@@ -0,0 +1,7 @@
+package com.benjamin;
+
+public class NextNumber {
+    public static int addition(int num) {
+        return num + 1;
+    }
+}
