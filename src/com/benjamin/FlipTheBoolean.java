29b9ea47b99edc84f3327f0fbbb45fac503ae0d6
Keith Benjamin <kjb528i@yahoo.com>
2022-01-04 17:40:50 -0500

    Added Flip the Boolean;

---
+++ src/com/benjamin/FlipTheBoolean.java
@@ -0,0 +1,10 @@
+package com.benjamin;
+
+public class FlipTheBoolean {
+    public static boolean reverse(boolean b) {
+        if( b == true){
+            return false;
+        }
+        return true;
+    }
+}
