0415d89e99b8b6b7801de5e9b84467080aa713f8
Keith Benjamin <kjb528i@yahoo.com>
2022-06-13 22:41:54 -0400

    Added - JAVA LOOP 1

--- src/com/benjamin/JavaLoops.java
+++
@@ -1,4 +0,0 @@
-package com.benjamin;
-
-public class JavaLoops {
-}
---
+++ src/com/benjamin/JavaLoops1.java
@@ -0,0 +1,15 @@
+package com.benjamin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+
+//    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void cal(int n){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
